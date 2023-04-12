8ba81583519cec4ace3c4df2a3eeca12d3ecdb4e
Steffen André Langnes <steffenl.sw@gmail.com>
2023-04-12 22:47:23 +0900

    Install uuid-dev

--- scripts/100-install-extra.sh
+++ scripts/100-install-extra.sh
@@ -4,5 +4,6 @@ apt-get update || exit 1
 apt-get install -y \
     gettext \
     git \
-    libgtk-3-dev || exit 1
+    libgtk-3-dev \
+    uuid-dev || exit 1
 rm -rf /var/lib/apt/lists/* || exit 1
