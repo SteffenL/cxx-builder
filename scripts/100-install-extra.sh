564651fc528e7c84a09b57868db567ab03e82458
Steffen André Langnes <steffenl.sw@gmail.com>
2023-04-14 16:01:21 +0900

    Install libboost-dev and libwxgtk3.0-gtk3-dev

--- scripts/100-install-extra.sh
+++ scripts/100-install-extra.sh
@@ -4,6 +4,8 @@ apt-get update || exit 1
 apt-get install -y \
     gettext \
     git \
+    libboost-dev \
     libgtk-3-dev \
+    libwxgtk3.0-gtk3-dev \
     uuid-dev || exit 1
 rm -rf /var/lib/apt/lists/* || exit 1
