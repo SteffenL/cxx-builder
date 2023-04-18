4dcd34fbb381c484ed9cf0e1607bfadf15815db7
Steffen André Langnes <steffenl.sw@gmail.com>
2023-04-18 14:48:35 +0900

    Fix wrong hash used with Ninja

--- scripts/102-install-ninja.sh
+++ scripts/102-install-ninja.sh
@@ -6,7 +6,7 @@ ninja_url="https://github.com/ninja-build/ninja/releases/download/v${ninja_versi
 
 temp_dir=$(mktemp --dir) || exit 1
 curl -sSLo "${temp_dir}/ninja.zip" "${ninja_url}" || exit 1
-echo "${cmake_hash} *${temp_dir}/ninja.zip" > "${temp_dir}/ninja.zip.hash" || exit 1
+echo "${ninja_hash} *${temp_dir}/ninja.zip" > "${temp_dir}/ninja.zip.hash" || exit 1
 shasum --check "${temp_dir}/ninja.zip.hash" || exit 1
 unzip -d /usr/local/bin "${temp_dir}/ninja.zip" || exit 1
 rm --recursive --dir "${temp_dir}" || exit 1
