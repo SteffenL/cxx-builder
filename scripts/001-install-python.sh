12025aeca3c6603b801e43dfe1c250d226b916ce
Steffen André Langnes <steffenl.sw@gmail.com>
2023-04-19 12:13:17 +0900

    Install python3-pip

--- scripts/001-install-python.sh
+++ scripts/001-install-python.sh
@@ -6,6 +6,7 @@ apt-get update || exit 1
 apt-get install -y \
     python3 \
     "python${python_version}" \
+    python3-pip \
     python3-venv || exit 1
 rm -rf /var/lib/apt/lists/* || exit 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 308 || exit 1
