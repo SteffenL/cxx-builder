787886bd88abf1020cf7c97b019b223e618a9927
Steffen André Langnes <steffenl.sw@gmail.com>
2023-04-19 12:20:14 +0900

    Install python3.8-venv

--- scripts/001-install-python.sh
+++ scripts/001-install-python.sh
@@ -5,9 +5,11 @@ python_version=$(echo "${1}" | grep --only-matching --extended-regexp '^[0-9]+\.
 apt-get update || exit 1
 apt-get install -y \
     python3 \
-    "python${python_version}" \
     python3-pip \
-    python3-venv || exit 1
+    python3-venv \
+    "python${python_version}" \
+    "python${python_version}-venv" \
+    || exit 1
 rm -rf /var/lib/apt/lists/* || exit 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 308 || exit 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 308 || exit 1
