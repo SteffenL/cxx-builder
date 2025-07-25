c20ea7b8bafa6399ea7f9040ea9d2d89d6e9ee2c
Steffen André Langnes <steffenl.sw@gmail.com>
2025-07-25 15:41:54 +0900

    Update Python install script
    Also install dev package.

--- scripts/001-install-python.sh
+++ scripts/001-install-python.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
+set -e
 
-python_version=$(echo "${1}" | grep --only-matching --extended-regexp '^[0-9]+\.[0-9]+') || exit 1
+python_version=$(echo "${1}" | grep --only-matching --extended-regexp '^[0-9]+\.[0-9]+')
+python_version_major=$(echo "${python_version}" | cut --delimiter . --field 1)
+python_version_minor=$(echo "${python_version}" | cut --delimiter . --field 2)
+python_version_int=$(printf '%d%02d' "${python_version_major}" "${python_version_minor}")
 
-apt-get update || exit 1
+apt-get update
 apt-get install -y \
-    python3 \
-    python3-pip \
-    python3-venv \
     "python${python_version}" \
+    "python${python_version}-dev" \
     "python${python_version}-venv" \
-    || exit 1
-rm -rf /var/lib/apt/lists/* || exit 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 308 || exit 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 308 || exit 1
+    "python${python_major_version}-pip" \
+   
+rm -rf /var/lib/apt/lists/*
+
+update-alternatives --install /usr/bin/python python "/usr/bin/python${python_version}" "${python_version_int}"
+update-alternatives --install /usr/bin/python3 python3 "/usr/bin/python${python_version}" "${python_version_int}"
+python3 --version | grep --fixed-strings "${python_version}" || (echo "Wrong Python version detected"; exit 1)
